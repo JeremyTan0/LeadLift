7ae17cb483645e91ac73f7c9117469d2b0ca1918
BaronViper <baron.viper10@gmail.com>
2025-07-07 22:52:05 -0400

    Update engine database path

--- backend/db/sessions.py
+++ backend/db/sessions.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-engine = create_engine("sqlite:///cache.db", echo=True)
+engine = create_engine("sqlite:///leadlift.db", echo=True)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
